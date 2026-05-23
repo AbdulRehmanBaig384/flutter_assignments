a2cbc5379b210f2640322a35420e383ce2d9b116
rehman <rehmanbaig456@gmail.com>
2026-05-24 01:42:35 +0500

    update api_constant

--- Assignment9/product_catalog_manager/lib/core/constants/api_constants.dart
+++ Assignment9/product_catalog_manager/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String apiToken = 'd89cdeabf5cb4a14a6011c750b32938a';
+  static const String apiToken = '67a75a76ea424b239c510e7a21424798';
   static const String baseUrl = 'https://crudcrud.com/api/$apiToken';
   static const String productsEndpoint = '/products';
 }
